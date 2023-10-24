8aef62a2123429b7cb1c00a57f31ba1fc8d3392a
Ezequiel Gómez Merlo <gomezmerloe@gmail.com>
2023-10-24 18:20:47 -0300

    added log

--- src/connectionDB.js
+++ src/connectionDB.js
@@ -2,6 +2,7 @@ import mysql from "mysql2/promise";
 
 const connectionDB = async () => {
   try {
+    console.log("DATABASE ENVIROMENT VARIABLE: " + process.env.DATABASE_URL);
     const connected = await mysql.createConnection(process.env.DATABASE_URL);
     return connected;
   } catch (error) {
