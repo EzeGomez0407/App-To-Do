4f9a0a0f7317f471191491a13f01586e190dd6d7
Ezequiel Gómez Merlo <gomezmerloe@gmail.com>
2023-10-24 16:42:34 -0300

    fixed enviroment variable

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -7,7 +7,7 @@ export const authOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackUrl: "http://localhost:3000/",
+      callbackUrl: process.env.NEXT_PUBLIC,
       returnTo: "/",
     }),
     Credentials({
