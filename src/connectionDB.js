8a232b4728da7558c9c9241e463bc332ee4f3a83
Ezequiel Gómez Merlo <gomezmerloe@gmail.com>
2023-10-24 18:15:57 -0300

    added log

--- src/connectionDB.js
+++ src/connectionDB.js
@@ -5,6 +5,7 @@ const connectionDB = async () => {
     const connected = await mysql.createConnection(process.env.DATABASE_URL);
     return connected;
   } catch (error) {
+    console.log("Error connecting to the DB: ", error);
     throw Error(error);
   }
 };
